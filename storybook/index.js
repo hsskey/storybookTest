ff1c855624114ea6cce7bded332e5938ad902e4a
cinos <cinos81@gmail.com>
2019-08-18 13:57:46 +0900

    Update index.js
    expo로 기동하시면 AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot); 는 필요가 없을것입니다.

--- storybook/index.js
+++ storybook/index.js
@@ -15,6 +15,6 @@ const StorybookUI = getStorybookUI({ port: 7007, host: 'localhost' });
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you can safely remove this line.
-AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
+// AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
 
 export default StorybookUI;
